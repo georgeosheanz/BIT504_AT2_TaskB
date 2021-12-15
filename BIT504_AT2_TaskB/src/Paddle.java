5787d0c1031b9e6e032f220effbc49db5723d50c
georgeosheanz <george.oshea@icloud.com>
2021-12-15 22:03:33 +1300

    Both paddles increased in size, paddles and text changed to colour RED

--- BIT504_AT2_TaskB/src/Paddle.java
+++ BIT504_AT2_TaskB/src/Paddle.java
@@ -6,9 +6,9 @@ import java.awt.Color;
 
 public class Paddle extends Sprite{
 	
-	private static final int PADDLE_WIDTH = 10;
-	private static final int PADDLE_HEIGHT = 100;
-	private static final Color PADDLE_COLOUR = Color.BLACK;
+	private static final int PADDLE_WIDTH = 20;
+	private static final int PADDLE_HEIGHT = 150;
+	private static final Color PADDLE_COLOUR = Color.RED;
 	private static final int DISTANCE_FROM_EDGE = 40;
 	
 	public Paddle(Player player, int panelWidth, int panelHeight) {
